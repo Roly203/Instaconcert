7303365fd8c1464d4cf42abed28d594bf915c2e1
student <student@codeacademy.org>
2012-07-27 20:29:24 -0500

    adding event scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instaconcert::Application.config.secret_token = '269d6cc3b651e694a0288450fadb32005130e3c03360e2cfd4064ae398795928432c59156aee34cf7224e7d00f714ce82edcb986923cc8286c806fa14d15ecb8'
