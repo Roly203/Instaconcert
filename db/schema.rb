bb84acace2f35b4185de2730287b2917bcca5979
pummer <adampumm@gmail.com>
2012-08-10 18:59:53 -0500

    fail

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(:version => 20120808034441) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+  
 
   create_table "events", :force => true do |t|
     t.string   "name"
